3f328fd301351dcea5fa111f9c98bf0b9fe50800
blackops <keerthigro321@gmail.com>
2024-09-28 13:09:59 +0530

    Add files via upload

---
+++ 0.1_strings.py
@@ -0,0 +1,33 @@
+
+string="this is string"
+string2='use single quotes also'
+string3="string in""this string""for use double quotes"
+string4="it's quotes inside a quotes use double quotes for use quote"
+multi_line=""""this is a multi line string
+            use three quotes for this multiline
+            string"""
+
+#get the character to call index position
+
+print(string[5])
+
+#looping throuhg a string use with a for loop
+
+for x in "apple":
+    print(x)
+
+#To get a length of string
+
+x=len(string)
+print(x)
+
+#check string use key word
+#return true or false
+
+print ("is" in string)    #iruka 
+
+if "is" in string:
+    print("yes")
+
+print("is"not in string)    #ilaya
+
---
+++ 0_data_types.py
@@ -0,0 +1,19 @@
+#Data types
+
+str_type = "this is string data type",
+int_type = 756
+float_type = 5.2
+complex_type =1j
+list_type = ["this is list data type","two","five",54]
+tuple_type = ("same as list ","its unchangeble","braces is diffirent",54)
+dict_type = {"key":"value",
+             "use curly braces":"for this type",
+             "inti":"54"}
+set_type = {"this","is","set data type",502}
+frozenset_type =({1,2,3,"donot allow dublicate","canot modify after creation"})
+bool_type = True    #return only rue or false
+bytes_type =b'string' #use b in the front of the value
+bytes_array_type = bytearray(4)
+memoryview_type = memoryview(bytes(4))
+none_type = None # onum ila 
+
